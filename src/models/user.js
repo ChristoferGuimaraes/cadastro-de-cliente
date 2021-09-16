77ce0596035307dab0654ce3280768980fbd6c1e
Christofer <christoferguima@gmail.comnam>
2021-09-15 23:40:12 -0300

    first commit

---
+++ src/models/user.js
@@ -0,0 +1,37 @@
+const mongoose = require('../database');
+const bcrypt = require('bcryptjs');
+
+const UserSchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        require: true,
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false, //pra quando buscar no banco, essa informação não venha no array de usuários
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+
+});
+
+UserSchema.pre('save', async function(next) {
+    const hash = await bcrypt.hash(this.password, 10);
+    this.password = hash;
+
+    next();
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
